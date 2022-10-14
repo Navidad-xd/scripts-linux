6d878eba6136bd19c600a97243310c3422d44c62
Sergio Díaz <104896798+Navidad-xd@users.noreply.github.com>
2022-10-14 12:25:52 +0100

    Update adivina.sh
    No lo cambio más, estoy cansado, me quiero morir

--- adivina.sh
+++ adivina.sh
@@ -6,9 +6,20 @@ read -p "Introduce un número entre 1 y 100: " numero
 conteo=$(($conteo + 1))
 if [ "$numero" == "$aleatorio" ]
 then
+if which figlet > /dev/null
+then
 figlet GANASTE!
 echo "Número de intentos: $conteo"
 exit 1
+else
+echo "  ____    _    _   _    _    ____ _____ _____ _
+ / ___|  / \  | \ | |  / \  / ___|_   _| ____| |
+| |  _  / _ \ |  \| | / _ \ \___ \ | | |  _| | |
+| |_| |/ ___ \| |\  |/ ___ \ ___) || | | |___|_|
+ \____/_/   \_\_| \_/_/   \_\____/ |_| |_____(_)"
+echo "Número de intentos: $conteo"
+exit 1
+fi
 else
 if [ "$numero" -gt "$aleatorio" ]
 then
