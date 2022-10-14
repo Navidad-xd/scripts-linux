d740e74ccf9f0992fec4d5cacac569cc59f5e255
Sergio Díaz <104896798+Navidad-xd@users.noreply.github.com>
2022-10-14 12:13:54 +0100

    Update adivina.sh

--- adivina.sh
+++ adivina.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-aleatorio=$(($RANDOM % 100)) 
+aleatorio=$(($RANDOM % 100 + 1)) 
 while true
 do
 read -p "Introduce un número entre 1 y 100: " numero
 conteo=$(($conteo + 1))
 if [ "$numero" == "$aleatorio" ]
 then
+if which cowsay > /dev/null
+then
 figlet GANASTE!
+echo "Número de intentos: $conteo"
+else
+echo "  ____    _    _   _    _    ____ _____ _____ _
+ / ___|  / \  | \ | |  / \  / ___|_   _| ____| |
+| |  _  / _ \ |  \| | / _ \ \___ \ | | |  _| | |
+| |_| |/ ___ \| |\  |/ ___ \ ___) || | | |___|_|
+ \____/_/   \_\_| \_/_/   \_\____/ |_| |_____(_)"
 echo "Número de intentos: $conteo"
 exit 1
 else
