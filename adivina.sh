55f35b9b83ef126e3664ddde555c0eafb1af36a4
Sergio Díaz <104896798+Navidad-xd@users.noreply.github.com>
2022-10-14 12:16:54 +0100

    Update adivina.sh

--- adivina.sh
+++ adivina.sh
@@ -10,13 +10,6 @@ if which figlet > /dev/null
 then
 figlet GANASTE!
 echo "Número de intentos: $conteo"
-else
-echo "  ____    _    _   _    _    ____ _____ _____ _
- / ___|  / \  | \ | |  / \  / ___|_   _| ____| |
-| |  _  / _ \ |  \| | / _ \ \___ \ | | |  _| | |
-| |_| |/ ___ \| |\  |/ ___ \ ___) || | | |___|_|
- \____/_/   \_\_| \_/_/   \_\____/ |_| |_____(_)"
-echo "Número de intentos: $conteo"
 exit 1
 else
 if [ "$numero" -gt "$aleatorio" ]
