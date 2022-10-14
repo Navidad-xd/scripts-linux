2b6fad5355d97f519b3f8e6e4c2b3c55dd3aa968
Sergio Díaz <104896798+Navidad-xd@users.noreply.github.com>
2022-10-14 11:53:42 +0100

    Update adivina.sh

--- adivina.sh
+++ adivina.sh
@@ -6,8 +6,12 @@ read -p "Introduce un número entre 1 y 100: " numero
 conteo=$(($conteo + 1))
 if [ "$numero" == "$aleatorio" ]
 then
-echo "Has acertado"
-echo "El número de intentos es de $conteo"
+echo "  ____    _    _   _    _    ____ _____ _____ _"
+echo " / ___|  / \  | \ | |  / \  / ___|_   _| ____| |"
+echo "| |  _  / _ \ |  \| | / _ \ \___ \ | | |  _| | |"
+echo "| |_| |/ ___ \| |\  |/ ___ \ ___) || | | |___|_|"
+echo " \____/_/   \_\_| \_/_/   \_\____/ |_| |_____(_)"
+echo "Número de intentos: $conteo"
 exit 1
 else
 if [ "$numero" -gt "$aleatorio" ]
